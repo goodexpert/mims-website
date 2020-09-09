b51e42da2bbeb01c3e16c829e824a6fd2121aa12
Steve Park <seongwuk.park@gmail.com>
2020-09-09 16:00:28 +0900

    Merge Paper Dashboard PRO React

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import {
+  Router, Route, Switch, Redirect,
+} from "react-router-dom";
+
+import AuthLayout from "layouts/Auth.jsx";
+import AdminLayout from "layouts/Admin.jsx";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.1.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+import * as serviceWorker from "./serviceWorker";
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={hist}>
+    <Switch>
+      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Redirect to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
